10e8ca84e32854d65fc30dc5629c48f65de15e4f
mostafa <mostafamahimakash384@gmail.com>
2025-04-11 20:54:32 +0600

    videos has been shown category wise

--- test.js
+++ test.js
@@ -1,11 +1,21 @@
-const time = (time) => {
-    const hour = parseInt(time / 3600);
-    const remainingSecond = time % 3600;
-    const min = parseInt(remainingSecond / 60);
-    const second = remainingSecond % 60;
-    return (`${hour} hour ${min} min ${second} second ago`);
-}
+// const time = (time) => {
+//     const hour = parseInt(time / 3600);
+//     const remainingSecond = time % 3600;
+//     const min = parseInt(remainingSecond / 60);
+//     const second = remainingSecond % 60;
+//     return (`${hour} hour ${min} min ${second} second ago`);
+// }
 
 
 
+// if(num < 100){
+// console.log("boro vai");
+// }
+// else{
+//     console.log('choto vai');
+// }
 
+
+const num = 10;
+
+num<100 ? console.log('boro vai'):  console.log('choto vai');
--- video.js
+++ video.js
@@ -1,3 +1,6 @@
+
+
+//time function to get the time , time conversion
 const time = (time) => {
     const hour = parseInt(time / 3600);
     const remainingSecond = time % 3600;
@@ -8,39 +11,47 @@ const time = (time) => {
 
 
 
-
-
-
-
-
-
-
+//categories loading
 const loadCategories = () => {
     fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
         .then(res => res.json())
-        .then(data => displayCategories(data.categories))
-        .catch(error => console.log(error));
+        .then(data => displayCategories(data.categories)) //call the displayCategories 
+        .catch(error => console.log(error)); //catch the  error if there any 
 }
 
 const displayCategories = (categories) => {
     for (const category of categories) {
-        //create a button
-        const button = document.createElement('button');
-        button.classList.add('btn');
-        button.classList.add('mx-5');
-        button.innerText = category.category;
+        //create the dyanmic buttons for categories 
+        const buttonContainer = document.createElement('div');
+        buttonContainer.innerHTML = `
+         <button onClick= "loadCategoryVideos(${category.category_id})" class="btn mr-5">
+         ${category.category}
+         </button>
+        `
 
         const categoryDiv = document.getElementById('category-section')
-        categoryDiv.append(button);
+        categoryDiv.append(buttonContainer); //music,drawing,comedy
     }
 }
 
 
+//load category wise videos
+function loadCategoryVideos(categoryId){
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${categoryId}`)
+    .then(res => res.json())
+    .then(data => displayVideos(data.category)) //call the displayVideos function to show all videos
+    .catch(error => console.log(error))
+
+}
+
+
+
+//load all the videos
 const loadVideos = () => {
 
     fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
         .then(res => res.json())
-        .then(data => displayVideos(data.videos))
+        .then(data => displayVideos(data.videos)) //call the displayVideos function to show all videos
         .catch(error => console.log(error))
 
 }
@@ -48,33 +59,43 @@ const loadVideos = () => {
 
 const displayVideos = (videos) => {
     const videosContainer = document.getElementById('videos')
+    videosContainer.innerHTML = ""; //clear the videos container
+
+
+//if there is no videos , length then it will show nothing
+    if(videos.length == 0){
+        videosContainer.classList.remove('grid')
+        videosContainer.innerHTML = `
+        
+<div class=" h-[400px] flex flex-col justify-center items-center mx-auto">
+    <img class= "h-[150px] " src="assets/Icon.png" alt="">
+    <h1 class=" p-5 text-xl text-center font-extrabold w-10/12">Opss! Sorry there is no content here</h1>
+</div>
+        
+        `
+        return;
+    }
+
+    else{
+        videosContainer.classList.add('grid');
+    }
+
     for (const video of videos) {
         console.log(video)
         const card = document.createElement('div')
         card.classList.add('card')
-        card.innerHTML = `
 
-
-
+        //card system
+        card.innerHTML = ` 
 
           <figure class="p-1 h-[200px] relative">
    
       <img src="${video.thumbnail}"
       alt="Shoes"
       class="rounded-xl w-full h-full object-cover" />
-
-
-      ${video.others.posted_date?.length == 0 ? "" : `<span class="absolute right-2 bottom-2 text-white bg-black rounded p-2 m-3" >${time(video.others.posted_date)} </span>`}
-
-
-
-     
+      ${video.others.posted_date?.length == 0 ? "" : `<span class="absolute right-2 bottom-2 font-light text-xs text-white bg-black rounded p-2 m-3" >${time(video.others.posted_date)} </span>`}
 
           </figure>
-
-
-
-
 
  <div class="flex py-3 items-center"> 
    <div class="w-[30px] h-[30px] mr-3">
@@ -88,7 +109,7 @@ const displayVideos = (videos) => {
          <div class="flex items-center ">
           <p class="font-light text-sm mr-5"> ${video.authors[0].profile_name}</p>
 
-          ${video.authors[0].verified ? '<img src="https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png" class="w-2/20" alt="">' : ""}
+          ${video.authors[0]?.verified ? '<img src="https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png" class="w-2/20" alt="">' : ""}
 
           
          </div>
